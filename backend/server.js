0bca056b932d298ed481b0876f0cc66b1d99ab30
Yohannes-J <yohannesjohn126@gmail.com>
2025-06-30 20:50:33 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://mint-planmonitoring.onrender.com/",
+    origin: "https://mint-planmonitoring.onrender.com",
     credentials: true,
   })
 );
@@ -101,7 +101,7 @@ app.use("/api/worker-performance", WorkerPerformanceRouter);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://mint-planmonitoring.onrender.com/",
+    origin: "https://mint-planmonitoring.onrender.com",
     credentials: true,
   },
 });
