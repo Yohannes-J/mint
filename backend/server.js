07cd776784d05950c322c41189c7b1516a551138
Yohannes-J <yohannesjohn126@gmail.com>
2025-07-01 15:51:32 +0200

    chngess

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://plan-monitoring.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
@@ -101,7 +101,7 @@ app.use("/api/worker-performance", WorkerPerformanceRouter);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://plan-monitoring.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
   },
 });
